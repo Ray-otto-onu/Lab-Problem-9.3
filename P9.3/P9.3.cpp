9df51c4b35c9f095a25ddb5f9329c09567904036
RAY OTTO <rjotto123@gmail.com>
2023-11-02 12:22:58 -0400

    completed displayRun

--- P9.3/P9.3.cpp
+++ P9.3/P9.3.cpp
@@ -1,1 +1,36 @@
-.
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+bool displayRun(int values[], int size);
+
+int main(void) {
+    srand(static_cast<unsigned>(time(0)));
+    int size = 20;
+    int values[20];
+
+
+    if (displayRun(values, size)) {
+        cout << "has run: ";
+    }
+    else {
+        cout << "no run: ";
+    }
+ 
+    for (int i = 0; i < size; i++) {
+        values[i] = (rand() % 6) + 1;
+        cout << values[i] << " ";
+    }
+
+    return 0;
+}
+
+bool displayRun(int values[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (values[i] == values[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+}
